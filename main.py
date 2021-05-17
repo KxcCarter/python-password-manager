8e5bdb9363e072da7be9c9cd050ad1dd62e081ec
Kenneth Carter <kxccarter@gmail.com>
2021-05-17 16:49:49 -0500

    updates doc strings

--- main.py
+++ main.py
@@ -6,6 +6,12 @@ import pyperclip
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 def generate_password():
+    """
+    Generates a random password.
+    Inserts password into password field.
+    Copies password to clipboard.
+
+    """
     letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
     numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
     symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
@@ -26,8 +32,12 @@ def generate_password():
 
 def save_password_info():
     """
+    Gets input from website, email, and password fields.
+    Checks to make sure all fields have been filled.
+    If not, returns.
+    If yes, saves data and clears form.
     "a" means it appends to the file
-    :return:
+
     """
     website = website_entry.get()
     email = email_entry.get()
